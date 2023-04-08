131298a0511a51d3a3c9fad5bc19f60466b08993
Davi Barci <davibarci0208@gmail.com>
2023-04-07 21:03:21 -0300

    Adicionado o método post que recebe informações de um tweet feito

--- src/index.js
+++ src/index.js
@@ -6,10 +6,22 @@ app.use(cors());
 app.use(express.json());
 
 const usuariosServidor = [];
+const tweetsServidor = [];
 
 app.post('/sign-up', (req, res) => {
     usuariosServidor.push({username: req.body.username, avatar: req.body.avatar});
     res.send("OK");
+});
+
+app.post('/tweets', (req, res) => {
+    const usuario = usuariosServidor.find(elem => elem.username === req.body.username);
+
+    if (usuario){
+        tweetsServidor.push({username: req.body.username, tweet: req.body.tweet});
+        return res.send("OK");
+    }
+
+    res.send("UNAUTHORIZED");
 });
 
 const PORT = 5000;
