574c05494492c7758e4c16c931551d770e78d059
Davi Barci <davibarci0208@gmail.com>
2023-04-09 22:19:58 -0300

    Adicionada a paginação no método get da rota /tweets

--- src/index.js
+++ src/index.js
@@ -10,22 +10,22 @@ const tweetsServidor = [];
 
 app.post('/sign-up', (req, res) => {
     if (!req.body.username || typeof req.body.username !== "string"
-    || !req.body.avatar || typeof req.body.avatar !== "string"){
+        || !req.body.avatar || typeof req.body.avatar !== "string") {
         return res.status(400).send("Todos os campos são obrigatórios!");
     }
-    usuariosServidor.push({username: req.body.username, avatar: req.body.avatar});
+    usuariosServidor.push({ username: req.body.username, avatar: req.body.avatar });
     return res.status(201).send("OK");
 });
 
 app.post('/tweets', (req, res) => {
     if (!req.body.username || typeof req.body.username !== "string"
-    || !req.body.tweet || typeof req.body.tweet !== "string"){
+        || !req.body.tweet || typeof req.body.tweet !== "string") {
         return res.status(400).send("Todos os campos são obrigatórios!");
     }
     const usuario = usuariosServidor.find(elem => elem.username === req.body.username);
 
-    if (usuario){
-        tweetsServidor.push({username: req.body.username, avatar: usuario.avatar, tweet: req.body.tweet});
+    if (usuario) {
+        tweetsServidor.push({ username: req.body.username, avatar: usuario.avatar, tweet: req.body.tweet });
         return res.status(201).send("OK");
     }
 
@@ -33,14 +33,25 @@ app.post('/tweets', (req, res) => {
 });
 
 app.get('/tweets', (req, res) => {
-    res.send(tweetsServidor.slice(-10));
+    const page = parseInt(req.query.page);
+
+    if (isNaN(page)) {
+        res.send(tweetsServidor.reverse().slice(10));
+    } else if (page <= 0) {
+        res.status(400).send("Informe uma página válida!");
+    } else {
+        const limite = 10;
+        const iniIndex = (page - 1) * limite;
+        res.send(tweetsServidor.reverse().slice(iniIndex, iniIndex+limite));
+    }
+
 });
 
 app.get('/tweets/:username', (req, res) => {
     const username = req.params.username;
     const userTweets = tweetsServidor.filter(elem => elem.username === username);
 
-    res.send(userTweets);
+    res.send(userTweets.reverse());
 });
 
 const PORT = 5000;
